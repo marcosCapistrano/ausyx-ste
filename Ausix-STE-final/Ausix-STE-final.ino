8834769c73ddd51fe36737ee956e1b6fa58c4228
Marcos Capistrano <mcapistrano50@gmail.com>
2022-08-03 16:09:35 -0300

    Adicionado sinal de wifi

--- Ausix-STE-final/Ausix-STE-final.ino
+++ Ausix-STE-final/Ausix-STE-final.ino
@@ -15,19 +15,43 @@ bool isAware = false; //Guarda se alguem apertou o botão de silenciar
 int temperatura = 0; // Valor recebido no WebSocketServer, enviado pelo sensor no ESP8266
 int tempLimite = 100; // Valor setado pelo potenciometro
 
+uint32_t lastComm = millis() / 1000;
+bool hasCommOnce = false;
+
 void setup(void) {
   Serial.begin(115200);
   
-  server_setup();
   lcd_setup();
   buzzer_setup();
   mutebutton_setup();
+
+  xTaskCreatePinnedToCore(
+      server_task, /* Function to implement the task */
+      "Task1", /* Name of the task */
+      10000,  /* Stack size in words */
+      NULL,  /* Task input parameter */
+      5,  /* Priority of the task */
+      NULL,  /* Task handle. */
+      0); /* Core where the task should run */
 }
 
 void loop(void) {
   potentiometer_loop();
-  server_loop();
-  
   lcd_draw();
   buzzer_loop();
+}
+
+/*
+   Esta função existe para garantir que o teste: state_manager_get(TEMP_MASS) <= a - b
+   Quando a < b, a subtração dos dois irá fazer o resultado causar um WRAP e bugar o resultado
+*/
+uint32_t safe_subtraction(uint32_t a, uint32_t b) {
+  uint32_t diff;
+  if (a < b) {
+    diff = 0;
+  } else {
+    diff = a - b;
+  }
+
+  return diff;
 }
